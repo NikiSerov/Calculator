4c8ddb1a9af2008de0d80ecb0d5c24726d47d0d9
nikitaserov <nikiserov1997js@gmail.com>
2022-11-01 22:01:41 +0400

    add handleOperatorClick

--- src/js/script.js
+++ src/js/script.js
@@ -2,30 +2,36 @@ const calcGrid = document.querySelector(".calculator-grid");
 const clearBtn = document.querySelector(".clear");
 const allClearBtn = document.querySelector(".allClear");
 const equalBtn = document.querySelector(".equal");
-
-function handleClick(e) {
-    const target = e.target;
-    if (target.classList.contains("number") || target.classList.contains("operator")) {
-        let inputValue = document.getElementById("output").value;
-        inputValue += target.textContent;
-        document.getElementById("output").value = inputValue;
-        const parsed = parseInt(target.textContent);
-        if (isNaN(parsed)) {
-            saveOperator(target.textContent);
-        } else {
-            saveNumber(parsed);
-        }
-    }
-}
-
-calcGrid.addEventListener("click", handleClick);
-equalBtn.addEventListener("click", equalClick);
-allClearBtn.addEventListener("click", allClear);
-clearBtn.addEventListener("click", clear)
+const output = document.querySelector("#output");
 
 let num1 = '';
 let num2 = '';
 let op = '';
+
+function handleClick(e) {
+    const target = e.target;
+
+    if (target.classList.contains("number")) {
+        outputValue(target);
+
+        setGlobalVariables(target.textContent);
+    }
+
+    if (target.classList.contains("operator")) {
+        if (handleOperatorClick(target)) {
+            outputValue(target);
+        }
+        
+        setGlobalVariables(target.textContent);
+    }
+}
+
+function outputValue(target) {
+    let inputValue = output.value;
+    
+    inputValue += target.textContent;
+    output.value = inputValue;
+}
 
 function saveNumber(number) {
     if (op === '') {
@@ -57,13 +63,69 @@ function equalClick() {
             result = num1 / num2;
             break;
     }
-    document.getElementById("output").value = result;
+    output.value = result;
+    clearGlobalVariables();
+    num1 = result.toString();
 }
 
 function allClear() {
-    document.getElementById("output").value = '';
+    output.value = '';
+    clearGlobalVariables();
 }
 
 function clear() {
-    document.getElementById("output").value = document.getElementById("output").value.slice(0, -1);
+    output.value = output.value.slice(0, -1);
+    if (op === '') {
+        num1 = num1.slice(0, -1);
+    }
+
+    if (num2 === '' && op !== '') {
+        op = '';
+    }
+
+    if (num2 !== '') {
+        num2 = num2.slice(0, -1);
+    }
+
+    console.log(num1, num2, op);
 }
+
+function handleOperatorClick(target) {
+    const operators = ['+', '-', '*', '/'];
+    let result = true;
+
+    for (let i = 0; i < output.value.length; i++) {
+        if (operators.includes(output.value[i])) {
+            result = false;
+            break;
+        }
+    }
+
+    if (operators.includes(output.value[output.value.length - 1])) {
+        output.value = output.value.slice(0, -1);
+        result = true;
+    }
+    return result;
+}
+
+function setGlobalVariables (symbol) {
+    console.log(symbol, 'huy');
+    const parsed = parseInt(symbol);
+
+        if (isNaN(parsed)) {
+            saveOperator(symbol);
+        } else {
+            saveNumber(parsed);
+        }
+}
+
+function clearGlobalVariables() {
+    num1 = '';
+    num2 = '';
+    op = '';
+}
+
+calcGrid.addEventListener("click", handleClick);
+equalBtn.addEventListener("click", equalClick);
+allClearBtn.addEventListener("click", allClear);
+clearBtn.addEventListener("click", clear)
